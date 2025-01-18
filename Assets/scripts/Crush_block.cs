340e6737faba5f60465cb99750096ec20784b587
chesterh <nastya>
2025-01-18 16:29:18 +0300

    asd'

--- Assets/scripts/Crush_block.cs
+++ Assets/scripts/Crush_block.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Crush_block : MonoBehaviour, IPointerClickHandler
-{   
+{   public bool canCrush;
     public GameObject breakEffect; // частицы 
     public string obj_name; // имя объекта под которым нужно будет сохранить текстуры
     private Material matDefault; // материал на объекте изначально 
@@ -13,19 +13,23 @@ public class Crush_block : MonoBehaviour, IPointerClickHandler
     public int count = 0;
     public int max_count;
     public void OnPointerClick(PointerEventData eventData){
-        GameObject explosion = Instantiate(breakEffect, this.gameObject.transform.position, Quaternion.identity);
-        explosion.GetComponent<ParticleSystem>().Play();
-        count += 1;
-        SpriteRend.material = matCrash;
-        if (count >= max_count){
-            Destroy(gameObject);
-            
+        if (canCrush == true){
+            GameObject explosion = Instantiate(breakEffect, this.gameObject.transform.position, Quaternion.identity);
+            explosion.GetComponent<ParticleSystem>().Play();
+            count += 1;
+            SpriteRend.material = matCrash;
+            if (count >= max_count){
+                Destroy(gameObject);
+                
+            }
+            Destroy(explosion, explosion.GetComponent<ParticleSystem>().main.startLifetime.constantMax);
         }
-        Destroy(explosion, explosion.GetComponent<ParticleSystem>().main.startLifetime.constantMax);
+
     }
     // Start is called before the first frame update
     void Start()
     {
+        
         SpriteRend = GetComponent<SpriteRenderer>();
         string materialName = obj_name + "Crash";
         matCrash = Resources.Load(materialName, typeof(Material)) as Material;
