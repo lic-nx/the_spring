19eb6f76456add739e37f7e4afc1b81c0b8660ef
chesterh <nastya>
2025-01-23 19:28:09 +0300

    add textures

--- Assets/scripts/Line_rendered.cs
+++ Assets/scripts/Line_rendered.cs
@@ -21,18 +21,22 @@ public class Line_rendered : MonoBehaviour
         if (points.Count > 0)
         {
             points[points.Count-1] = point;
+            lr.SetPosition(lr.positionCount-1, point);
         }
     }
 
 // добавляет точку
     public void SetUpLine(Vector3 point)
-    {   Debug.Log("lr before change" + lr.positionCount);
-        lr.positionCount++;
-        Debug.Log("lr after");
-        Debug.Log(lr.positionCount);
+    {   
+    // Debug.Log("lr before change" + lr.positionCount);
+        
+    //     Debug.Log("lr after");
+    //     Debug.Log(lr.positionCount);
 
         points.Add(point);
         Debug.Log(points.Count);
+        lr.positionCount++;
+        lr.SetPosition(lr.positionCount-1, points[points.Count-1]);
     }
 
     private void Update()
@@ -41,8 +45,8 @@ public class Line_rendered : MonoBehaviour
         for (int i = 0; i < points.Count; i++)
         {   
             pointsArray[i] = new Vector2(points[i].x, points[i].y);
-            lr.SetPosition(i, points[i]);
-            // edgeCollider.SetPosition(i, new Vector2(points[i].x, points[i].y));
+        //     lr.SetPosition(i, points[i]);
+        //     // edgeCollider.SetPosition(i, new Vector2(points[i].x, points[i].y));
             
         
         }
