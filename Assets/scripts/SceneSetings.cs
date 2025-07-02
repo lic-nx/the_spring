b770aa7572c87a7d62aef1621f5578b506863983
Your Name <you@example.com>
2025-07-02 20:33:41 +0300

    add end panel

--- Assets/scripts/SceneSetings.cs
+++ Assets/scripts/SceneSetings.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.PostProcessing;
 
 public class SceneSerings : MonoBehaviour
 {
-    public GameObject PausePanel, Inventory, tapEffect;
+    public GameObject PausePanel;
     public int level;
+    private PostProcessVolume ppVolume;
 
+    void Start() {
+        ppVolume = Camera.main.gameObject.GetComponent<PostProcessVolume>();
+    }
     public void PauseButtonPressed()
     {
+
         // вызываем остановку
         PausePanel.SetActive(true);
+        ppVolume.enabled = true;
         Time.timeScale = 0f;
     }
 
@@ -19,24 +26,28 @@ public class SceneSerings : MonoBehaviour
     {
         // вызываем продолжаем игру
         PausePanel.SetActive(false);
+        ppVolume.enabled = false;
         Time.timeScale = 1f;
     }
 
     public void ChangeScene(int scene)
     {
         SceneManager.LoadScene(scene);
+        ppVolume.enabled = false;
         Time.timeScale = 1f; 
     }
 
     public void RestartButtonPressed()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        ppVolume.enabled = false;
         Time.timeScale = 1f; 
     }
 
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        ppVolume.enabled = false;
 
     }
 }
--- Assets/scripts/WinPanel.cs
+++ Assets/scripts/WinPanel.cs
@@ -1,30 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.PostProcessing;
 
 public class WinPanel : MonoBehaviour
 {
     public GameObject winPanel;
+    private PostProcessVolume ppVolume;
+
+    void Start()
+    {
+        Debug.Log("Camera.main: " + (Camera.main ? Camera.main.name : "null"));
+        ppVolume = Camera.main.GetComponent<PostProcessVolume>();
+    }
 
     public void OnEventTriggered()
     {
-        winPanel.SetActive(true);
+        ppVolume = Camera.main.GetComponent<PostProcessVolume>();
+        ppVolume.enabled = true;
+        Debug.LogWarning("PostProcessVolume не найден. Эффект блюра не применён.");
+        if (winPanel != null)
+        {
+            winPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("winPanel не назначен!");
+        }
+
         Debug.Log("Игра завершена! Панель победы показана.");
     }
 
     public void Restart()
     {
+        //ppVolume.enabled = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1f;
     }
 
     public void Mainmenu()
     {
+        ppVolume.enabled = false;
         SceneManager.LoadScene(0);
         Time.timeScale = 1f;
     }
 
     public void NextLevel()
     {
+        ppVolume.enabled = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 1f;
     }
